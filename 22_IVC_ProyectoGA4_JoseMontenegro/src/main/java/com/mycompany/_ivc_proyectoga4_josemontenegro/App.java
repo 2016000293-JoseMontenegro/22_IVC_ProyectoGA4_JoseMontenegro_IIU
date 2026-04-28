92528bc44fa1551accd57641bcf49424c7082dd9
2016000293-JoseMontenegro <j.montenegro5adb@gmail.com>
2026-04-27 18:06:36 -0600

    Add files via upload

---
+++ 22_IVC_ProyectoGA4_JoseMontenegro/src/main/java/com/mycompany/_ivc_proyectoga4_josemontenegro/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._ivc_proyectoga4_josemontenegro;
+
+/**
+ *
+ * @author H4H7X
+ */
+public class App {
+
+    public static void main(String[] args) {
+        new jfrmMenu().setVisible(true);
+    }
+}
